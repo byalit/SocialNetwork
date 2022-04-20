ce71ea0ce102b09641d1feccc9504ce8b7fdab89
byalit <92673028+byalit@users.noreply.github.com>
2022-04-21 00:11:50 +0200

    13

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Ftse4.mm.bing.net%2Fth%3Fid%3DOIP.J2qXDNNag8xxvu51y4nzggHaHa%26pid%3DApi&f=1"></img>
+    </header>
+  );
+};
+export default Header;
---
+++ src/components/Profile.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Profile = () => {
+  return (
+    <div className="content">
+      <div>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.hdnicewallpapers.com%2FWalls%2FBig%2FVehicles%2FCycle_in_Farm_HD_Image.jpg&f=1&nofb=1"></img>
+      </div>
+      <div>
+        <img src="https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fpngimg.com%2Fuploads%2Fduck%2Fduck_PNG5036.png&f=1&nofb=1"></img>
+      </div>
+      <div>ava + discription</div>
+      <div>
+        My posts
+        <div>New posts</div>
+      </div>
+      <div>
+        <div> Post 1</div>
+        <div> Post 2</div>
+        <div> Post 3</div>
+      </div>
+    </div>
+  );
+};
+export default Profile;
