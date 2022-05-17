6bf06c19c628406d71de44dd0800f4aae83af692
byalit <92673028+byalit@users.noreply.github.com>
2022-05-18 00:03:08 +0200

    25

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -12,19 +12,31 @@ const MessageItem = (props) => {
     return <div className={s.message}>{props.message}</div>
 }
 const Dialogs = () => {
+    let dialogs = [
+        {id:1,name:'Charlotte'},
+        {id:2,name:'Andrew'},
+        {id:3,name:'John'},
+        {id:4,name:'Hazel'},
+        {id:5,name:'Luna'},
+        {id:6,name:'Theodore'}
+    ]
+    let messages = [
+        {id:1, message:'Hi'},
+        {id:1, message:'How old are you?'},
+        {id:1, message:'How are you?'},
+        {id:1, message:'Nien!'}
+
+    ]
+    let dialogsElements = dialogs.map(d => <DialogItem name={d.name} id={d.id} />)
+    let messagesElements = messages.map(m => <MessageItem message={m.message} />)
     return (
         <div className={s.dialogs}>
             <div className={s.dialogsItems}>
-                <DialogItem name='username1' id = '1'/>
-                <DialogItem name='username2' id = '2'/>
-                <DialogItem name='username3' id = '3'/>
+                {dialogsElements}
 
             </div>
             <div className={s.messages}>
-                <MessageItem message = 'Hi' />
-                <MessageItem message = 'How old are you?' />
-                <MessageItem message = 'How are you?' />
-                <MessageItem message = 'Nien!' />
+                {messagesElements}
 
             </div>
         </div>
